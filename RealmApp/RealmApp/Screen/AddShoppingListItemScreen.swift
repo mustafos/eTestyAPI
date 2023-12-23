64a5a4b8063824305d12ac3a5f2c771d2cb6fc98
Mustafa Bekirov <mustafos1999@gmail.com>
2023-12-23 11:37:37 +0100

    feat: relationships with delete & update

--- RealmApp/RealmApp/Screen/AddShoppingListItemScreen.swift
+++ RealmApp/RealmApp/Screen/AddShoppingListItemScreen.swift
@@ -11,15 +11,27 @@ import RealmSwift
 struct AddShoppingListItemScreen: View {
     
     @ObservedRealmObject var shoppingList: ShoppingList
+    var itemToEdit: ShoppingItem?
+    
     @Environment(\.dismiss) private var dismiss
     
     @State private var title: String = ""
     @State private var quantity: String = ""
     @State private var selectedCategory: String = ""
     
-    
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     let data = ["Produce", "Fruit", "Meet", "Condiments", "Beverages", "Snacks", "Dairy"]
+    
+    init(shoppingList: ShoppingList, itemToEdit: ShoppingItem? = nil) {
+        self.shoppingList = shoppingList
+        self.itemToEdit = itemToEdit
+        
+        if let itemToEdit = itemToEdit {
+            _title = State(initialValue: itemToEdit.title)
+            _quantity = State(initialValue: String(itemToEdit.quantity))
+            _selectedCategory = State(initialValue: itemToEdit.category)
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -44,12 +56,14 @@ struct AddShoppingListItemScreen: View {
                     .textFieldStyle(.roundedBorder)
                 
                 Button {
-                    // save the item
-                    let shopingItem = ShoppingItem()
-                    shopingItem.title = title
-                    shopingItem.quantity = Int(quantity) ?? 1
-                    shopingItem.category = selectedCategory
-                    $shoppingList.items.append(shopingItem)
+                    // save or update the item
+                    if let _ = itemToEdit {
+                        // update
+                        update()
+                    } else {
+                        // save
+                        save()
+                    }
                     
                     dismiss()
                     
@@ -63,6 +77,30 @@ struct AddShoppingListItemScreen: View {
                     .navigationTitle("Add Item")
             }
             .padding()
+        }
+    }
+    
+    private func save() {
+        let shopingItem = ShoppingItem()
+        shopingItem.title = title
+        shopingItem.quantity = Int(quantity) ?? 1
+        shopingItem.category = selectedCategory
+        $shoppingList.items.append(shopingItem)
+    }
+    
+    private func update() {
+        if let itemToEdit = itemToEdit {
+            do {
+                let realm = try Realm()
+                guard let objectToUpdate = realm.object(ofType: ShoppingItem.self, forPrimaryKey: itemToEdit.id) else { return }
+                try realm.write {
+                    objectToUpdate.title = title
+                    objectToUpdate.category = selectedCategory
+                    objectToUpdate.quantity = Int(quantity) ?? 1
+                }
+            } catch {
+                print(error)
+            }
         }
     }
 }
